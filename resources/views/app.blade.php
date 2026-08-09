4f4c1b13ff701adac8ff28246f804872b6fd4280
Rifqi <f8goodspoof@gmail.com>
2026-08-10 02:24:24 +0700

    segini dulu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title inertia>{{ config('app.name', 'SkillPath AI') }}</title>
+
     <link rel="icon" type="image/png" href="/assets/logoKibot.png?v=2">
     <link rel="shortcut icon" type="image/png" href="/assets/logoKibot.png?v=2">
     <link rel="apple-touch-icon" href="/assets/logoKibot.png?v=2">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Rifqi | SkillPath Ai</title>
+
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.tsx'])
+    @inertiaHead
 </head>
-<body>
-    <div>Hallo</div>
+<body class="font-sans antialiased">
+    @inertia
 </body>
 </html>
